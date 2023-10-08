a9ebc3bb3706afeff3e250eeb1f36b8df3c7ec46
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2023-10-08 14:45:56 +0300

    Update code.js

--- 5/code.js
+++ 5/code.js
@@ -1,29 +1,26 @@
-function onClick(event){
-    //event.preventDefault();
-    let number = document.getElementById("numberOfProducts").value;
-    let select = document.getElementById("Product");
-    let price = getPriceOfProduct(select.value);
-    let relult = document.getElementById("result");
-    result.innerHTML = "Результат: "+(parseInt(number)*price);
-    return false;
-}
-
-/*function checkNumber(num){
-    Проверка числа на отсутствие ненужных символов
-
-}*/
-
-function getPriceOfProduct(value){
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM fully loaded and parsed");
+    function onClick(){
+        let number = document.getElementById("numberOfProducts").value;
+        let select = document.getElementById("Product");
+        let price = getPriceOfProduct(select.value);
+        let relult = document.getElementById("result");
+        // Создание регулярного выражения для проверки
+        const pattern = /^[0-9]+$/;
+        const inputValue = number;
+        if (pattern.test(inputValue)){
+            console.log("Данные введены корректно.");
+            result.innerHTML = "Результат: "+(parseInt(number)*price);
+        } else {alert("Ошибка: недопустимые символы");}
+        return false;
+    }
+    //Цена товара
+    function getPriceOfProduct(value){
     if(value === "NaborNakleek") {return 150;}
     if(value === "PlushToy") {return 400;}
     if(value === "Pajamas") {return 600;}
     if(value === "Plaid") {return 800;}
-}
-
-
-//DOM
-window.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-    let b = document.getElementById("calculateButton");
-    b.addEventListener("click", onClick);
+    }
+    let calculateButton = document.getElementById("calculateButton");
+    calculateButton.addEventListener("click", onClick);
 });
