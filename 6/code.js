3e3d7cd2ba995c15c31547bfab79155bcc8fed22
TanyaOvs <144003852+TanyaOvs@users.noreply.github.com>
2023-10-14 14:55:21 +0300

    Update code.js

--- 6/code.js
+++ 6/code.js
@@ -1,22 +1,64 @@
 function onChange(event) {
     event.preventDefault();
 
+    //Количество товаров
     let number = document.getElementById("numberOfProducts").value;
-    let select = document.getElementById("Product");
-    let price = parseInt(select.value);
+    number = parseInt(number);
+    //Выбранная радиокнопка (value)
+    let selectedProduct = getSelectedProduct();
+    //Поле для вывода цены
     let result = document.getElementById("result");
-    console.log(select.value);
 
-    // Создание регулярного выражения для проверки
+    
+    //Первоначальная цена товара
+    let price = parseInt(selectedProduct);
+    // Создание регулярного выражения для проверки цены
     const pattern = /^[0-9]+$/;
     const inputValue = number;
+    let priceIsCorrect = false;
     if (pattern.test(inputValue)) {
         console.log("Данные введены корректно.");
-        result.innerHTML = "Результат: "+(parseInt(number)*price);
+        priceIsCorrect = true;
     } else 
     {
-        alert("Ошибка: недопустимые символы");
+        alert("Ошибка: количество товаров не указано или введены недопустимые символы");
     }
+    
+    //Изменение html файла при изменении товара
+    let selectForSecond = document.getElementById("selectForSecond");
+    let checkboxForThird = document.getElementById("checkboxForThird");
+    if(price == 400) {
+        selectForSecond.style.display ="none";
+        checkboxForThird.style.display ="none";
+        result.innerHTML = "Цена: "+(number*price);
+    }
+    if(price == 600) {
+        selectForSecond.style.display ="block";
+        checkboxForThird.style.display ="none";
+        //Подсчет стоимости
+        let selectedOption = document.getElementsByName("selectedOption");
+        selectedOption = parseInt(selectedOption[0].value);
+        result.innerHTML = "Цена: "+(number*(price + selectedOption));
+    }
+    if(price == 800) {
+        selectForSecond.style.display ="none";
+        checkboxForThird.style.display ="block";
+        //ДОДЕЛАТЬ НА ПАРЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕе
+    }
+
+}
+
+ //Функция для поиска выбранной радиокнопки
+function getSelectedProduct(){
+    let selectedProduct;
+    let Products = document.getElementsByName("product");
+    for(let i = 0; i < Products.length; i++) {
+        if(Products[i].checked){
+            selectedProduct = Products[i].value;
+            break;
+        }
+    }
+    return selectedProduct;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
